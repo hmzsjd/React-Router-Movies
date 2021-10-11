25bfaadacd6f686bd845de40d8f97e18024a29c3
hmzsjd <github@hamzasajid.com>
2021-10-11 11:49:33 -0700

    saved list appearing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import { Link, Route, Switch } from 'react-router-dom';
+
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +16,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +33,12 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+      <Route path="/">
+          <SavedList list={movieList} />
+        </Route>
+      </div>
     </div>
   );
 }
+
